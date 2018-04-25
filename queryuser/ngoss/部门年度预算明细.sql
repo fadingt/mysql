c54a145eee58184331c6cef0089147d3c721b9d1
fadingt <9zliuxingyu@gmail.com>
2018-04-25 15:31:30 +0800

    查询全面预算明细 周爽原始sql

--- queryuser/ngoss/部门年度预算明细.sql
+++ queryuser/ngoss/部门年度预算明细.sql
@@ -7,13 +7,13 @@ select
 from
 (
 	select 
-		budgetid, budgetno, budgetname, budgettype, budgetdescription,-- 预算编号 名称 类型 描述
+		budgetid, budgetno, budgetname, budgettype, budgetdescription,budgetnature,-- 预算编号 名称 类型 描述
 		SUBSTRING_INDEX(getLevelDeptNameByTian(departmentid),'-',2) departmentname , -- 所属部门名称
 		startdate, enddate, -- 开始结束日期 
 		budgetamount, -- 预算费用总金额
 		getusername(chargeperson) chargepersonname, getunitname(company) companyname-- 预算负责人 公司名称
 	from t_budget_deptannualbudget
-	where budgetnature = '1'
+
 ) t 
 LEFT JOIN
 (
@@ -39,16 +39,19 @@ left join (
 		select sum(workdays) as workdays,employeeid,targetid,yearmonth
 		from 	
 		( 
+-- 已通过的考勤
 				select 
 				FORMAT(sum(workhours)/8,2) as workdays,employeeid,targetid,left(yearmonthday,6) yearmonth 
 				from t_project_workhourmanager  
-				where workhourtype=2 
-				and status = 3 
+				where workhourtype=2 and status = 3 
 				GROUP BY employeeid,targetid,left(yearmonthday,6)
+
 				UNION ALL 
-				select FORMAT(sum(workhours)/8,2) as workdays,employeeid,targetid,left(yearmonthday,6) yearmonth from t_project_workhourmanager  
-				where workhourtype=2 
-				and status = 2 
+
+-- 待审批的考勤
+				select FORMAT(sum(workhours)/8,2) as workdays,employeeid,targetid,left(yearmonthday,6) yearmonth 
+				from t_project_workhourmanager  
+				where workhourtype=2 and status = 2 
 				GROUP BY employeeid,targetid,left(yearmonthday,6)
 				) x GROUP BY employeeid,targetid,yearmonth
 		 )a 
@@ -57,7 +60,16 @@ left join (
 	GROUP BY targetid,yearmonth
 ) x on x.targetid=t.budgetid
 
+left join
+(
+	SELECT 
+	budgetid, budgetno, budgetname, SUM(gz) gramty2, yearmonth
+	FROM `t_project_standardcost_sjgz`
+	GROUP BY budgetid, yearmonth
+) x2 on x2.budgetid = t.budgetid
+
 where 1=1
+and t.budgetnature = '1'
 -- {budgetno}
 -- {inputdate}
 -- {department}
