12993ad2d5a4f7ed1abdfccf171a7608c2fe14d7
fadingt <9zliuxingyu@gmail.com>
2018-05-21 11:04:17 +0800

    +快照日期 预计分配人 分配日期 union 历史数据

--- queryuser/ngoss/查询本部门的合同分配项目明细表.sql
+++ queryuser/ngoss/查询本部门的合同分配项目明细表.sql
@@ -1,35 +1,28 @@
 -- 查询本部门的合同分配项目明细表 contract_main_pro_br
 select * from (
-select 
-getcustname(m.firstparty) custname,
-t.contractno,
-t.contractname,
-t.id,
-t.contractid,
-m.deptid,
-t.orderid,
-t.pono,
-t.type,
-t.typename,
-t.stagename stagedescription,
-t.ybilldate,
-t.ybillamt,
-t.yrecedate,
-t.yreceamt,
-t.sbilldate,
-ifnull(t.sbillamt,0) sbillamt,
-ifnull(t.sreceamt,0) sreceamt,
-t.srecedate,
-(select deptid from  t_project_projectinfo where projectid=t.projectid) deptidp,
-(select projectno from  t_project_projectinfo where projectid=t.projectid) projectno,
-t.projectname,
-ifnull(t.f_sbillamt,0)  sallocatebillamt,
-ifnull(t.f_sreceamt,0)  sallocatepayamt,
-ifnull(t.f_ybillamt,0)   allocatebillamt,
-ifnull(t.f_yreceamt,0)  allocatepayamt
-
- from t_contract_stage_ysf_tian t
-left join t_contract_main m on m.contractid= t.contractid
+SELECT
+	ysf.*,
+	project.projectno, project.projectname, project.deptid deptidp,
+	getcustname(contract.firstparty) custname,  contract.deptid
+FROM(
+	select 	-- 历史数据
+		contractid, projectid,
+		contractno,contractname,id,orderid,pono,type,typename,stagename stagedescription,
+		ybilldate,ybillamt,yrecedate,yreceamt,sbilldate,srecedate,ifnull(sbillamt,0) sbillamt,ifnull(sreceamt,0) sreceamt,
+		ifnull(f_sbillamt,0) sallocatebillamt,	ifnull(f_sreceamt,0) sallocatepayamt,	ifnull(f_ybillamt,0) allocatebillamt,	ifnull(f_yreceamt,0) allocatepayamt,
+		f_fpperson,f_fpdate,insertdate -- 预计分配人 分配日期 快照日期
+	from t_contract_stage_ysf_tian_his
+	union all
+	select 	-- 当天数据
+		contractid, projectid,
+		contractno,contractname,id,orderid,pono,type,typename,stagename stagedescription,
+		ybilldate,ybillamt,yrecedate,yreceamt,sbilldate,srecedate,ifnull(sbillamt,0) sbillamt,ifnull(sreceamt,0) sreceamt,
+		ifnull(f_sbillamt,0) sallocatebillamt,	ifnull(f_sreceamt,0) sallocatepayamt,	ifnull(f_ybillamt,0) allocatebillamt,	ifnull(f_yreceamt,0) allocatepayamt,
+		f_fpperson,f_fpdate,insertdate
+	from t_contract_stage_ysf_tian
+) ysf
+left join t_project_projectinfo project on ysf.projectid = project.projectid
+left join t_contract_main contract on ysf.contractid = contract.contractid
 ) x
 where 1=1 
 -- and deptidp in (:roledatascope)
